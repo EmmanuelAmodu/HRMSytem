8a3f9636fb8808db4de29b72260521e496f408f9
developer1 <developer1@MY8760W>
2017-09-05 13:57:52 +0100

    incomplete pmsgroup edit component

--- src/app/payroll/admin/pmsgroups/pmsgroup-edit/pmsgroup-edit.component.ts
+++ src/app/payroll/admin/pmsgroups/pmsgroup-edit/pmsgroup-edit.component.ts
@@ -1,15 +1,93 @@
-import { Component, OnInit } from '@angular/core';
+﻿import { Component, ViewChild, Injector, Output, EventEmitter, ElementRef } from '@angular/core';
+import { ModalDirective } from 'ngx-bootstrap';
+import { PMSgroupServiceProxy, CustomerServiceProxy, PMSgroupEditDto, ComboboxItemDto } from '@shared/service-proxies/service-proxies';
+import { AppConsts } from '@shared/AppConsts';
+import { PayrollComponentBase } from "app/payroll/shared/payroll-component-base";
 
 @Component({
-  selector: 'app-pmsgroup-edit',
-  templateUrl: './pmsgroup-edit.component.html',
-  styleUrls: ['./pmsgroup-edit.component.css']
+    selector: 'pmsgroup-edit',
+    templateUrl: './pmsgroup-edit.component.html',
+    styleUrls: ['./pmsgroup-edit.component.css']
 })
-export class PmsgroupEditComponent implements OnInit {
+export class PmsgroupEditComponent extends PayrollComponentBase {
 
-  constructor() { }
+    @ViewChild('nameInput') nameInput: ElementRef;
+    @ViewChild('codeInput') codeInput: ElementRef;
+    @ViewChild('editModal') modal: ModalDirective;
 
-  ngOnInit() {
-  }
+    @Output() modalSave: EventEmitter<any> = new EventEmitter<any>();
+
+    public active: boolean = false;
+    public saving: boolean = false;
+    public loading: boolean = false;
+
+    customerComboboxItems: ComboboxItemDto[] = [];
+    pmsgroup: PMSgroupEditDto = new PMSgroupEditDto();
+    salaryCostCenterComboBoxItems: ComboboxItemDto[] = [];
+    staffModeComboBoxItems: ComboboxItemDto[] = [];
+    salaryAccountComboBoxItems: ComboboxItemDto[] = [];
+    salaryModelComboBoxItems: ComboboxItemDto[] = [];
+    taxModeComboBoxItems: ComboboxItemDto[] = []; 
+
+    constructor(
+        injector: Injector,
+        private _pmsgroupService: PMSgroupServiceProxy,
+        private _customerService: CustomerServiceProxy
+    ) {
+        super(injector);
+    }
+
+    getCustomerComboboxItems(NameFilter: string, BusinessSectorId: number, Sorting?: string, MaxResultCount?: number, SkipCount?: number) {
+        this.loading = true;
+        this._customerService.getCustomerComboBoxItems(
+            NameFilter, BusinessSectorId, Sorting, MaxResultCount, SkipCount
+        ).subscribe(result => {
+            this.customerComboboxItems = result.items;
+            console.log(result.items);
+            this.loading = false;
+        });
+    }
+
+    show(accountId?: number): void {
+        this.active = true;
+        this.loading = true;
+        this._pmsgroupService.getPMSgroupForEdit(accountId).subscribe(result => {
+            this.pmsgroup = result;
+            this.salaryCostCenterComboBoxItems = result.salaryCostCenterComboBoxItems;
+            this.staffModeComboBoxItems = result.staffModeComboBoxItems;
+            this.salaryAccountComboBoxItems = result.salaryAccountComboBoxItems;
+            this.salaryModelComboBoxItems = result.salaryModelComboBoxItems;
+            this.taxModeComboBoxItems = result.taxModeComboBoxItems;
+            this.loading = false;
+            this.modal.show();
+        });
+    }
+
+    setFocus(): void {
+        if (this.pmsgroup.id)
+            $(this.nameInput.nativeElement).focus();
+        else
+            $(this.codeInput.nativeElement).focus();
+    }
+
+    save(): void {
+        var input = new PMSgroupEditDto();
+        input = this.pmsgroup;
+        console.log(input);
+        this.saving = true;
+        this._pmsgroupService.createOrUpdatePMSgroup(input)
+            .finally(() => this.saving = false)
+            .subscribe(() => {
+                this.notify.info(this.l('SavedSuccessfully'));
+                this.close();
+                this.modalSave.emit(null);
+            });
+    }
+
+    close(): void {
+        this.active = false;
+        this.modal.hide();
+    }
 
 }
+
--- src/app/payroll/admin/pmsgroups/pmsgroup-list/pmsgroup-list.component.ts
+++ src/app/payroll/admin/pmsgroups/pmsgroup-list/pmsgroup-list.component.ts
@@ -1,15 +1,81 @@
-import { Component, OnInit } from '@angular/core';
+﻿import { Component, OnInit, AfterViewInit, Injector, ViewEncapsulation, ViewChild } from '@angular/core';
+import { appModuleAnimation } from '@shared/animations/routerTransition';
+import { AppConsts } from '@shared/AppConsts';
+import { PMSgroupServiceProxy , PMSgroupListDto } from '@shared/service-proxies/service-proxies';
+import { FileDownloadService } from '@shared/utils/file-download.service';
+import { PmsgroupEditComponent } from "app/payroll/admin/pmsgroups/pmsgroup-edit/pmsgroup-edit.component";
+import { DataTable } from 'primeng/components/datatable/datatable';
+import { Paginator } from 'primeng/components/paginator/paginator';
+import { LazyLoadEvent } from 'primeng/components/common/lazyloadevent';
+import { PayrollComponentBase } from "app/payroll/shared/payroll-component-base";
 
 @Component({
-  selector: 'app-pmsgroup-list',
-  templateUrl: './pmsgroup-list.component.html',
-  styleUrls: ['./pmsgroup-list.component.css']
+    selector: 'pmsgroup-list',
+    templateUrl: './pmsgroup-list.component.html',
+    styleUrls: ['./pmsgroup-list.component.css'],
+    encapsulation: ViewEncapsulation.None,
+    animations: [appModuleAnimation()],
+    providers: [PMSgroupServiceProxy]
 })
-export class PmsgroupListComponent implements OnInit {
+export class PmsgroupListComponent extends PayrollComponentBase {
 
-  constructor() { }
+    @ViewChild('editPMSgroupModal') editModal: PmsgroupEditComponent;
+    @ViewChild('dataTable') dataTable: DataTable;
+    @ViewChild('paginator') paginator: Paginator;
 
-  ngOnInit() {
-  }
+    constructor(
+        injector: Injector,
+        private _pmsgroupService: PMSgroupServiceProxy,
+        private _fileDownloadService: FileDownloadService
+    ) {
+        super(injector);
+    }
 
+    getPMSgroups(event?: LazyLoadEvent): void {
+        this.primengDatatableHelper.showLoadingIndicator();
+        this._pmsgroupService.getPMSgroups(
+            "",
+            this.primengDatatableHelper.getSorting(this.dataTable),
+            this.primengDatatableHelper.getMaxResultCount(this.paginator, event),
+            this.primengDatatableHelper.getSkipCount(this.paginator, event)
+        ).subscribe((data) => {
+            this.primengDatatableHelper.totalRecordsCount = data.totalCount;
+            this.primengDatatableHelper.records = data.items;
+            this.primengDatatableHelper.hideLoadingIndicator();
+        });
+    }
+
+    editRecord(id: number): void {
+        this.editModal.show(id);
+    }
+
+    deleteRecord(id: number): void {
+        if (id) {
+            this._pmsgroupService.getPMSgroupForEdit(id).subscribe(result => {
+                this.message.confirm(
+                    this.l('PMSgroupDeleteWarningMessage', result.name),
+                    (isConfirmed) => {
+                        if (isConfirmed) {
+                            this._pmsgroupService.deletePMSgroup(id)
+                                .subscribe(() => {
+                                    this.getPMSgroups();
+                                    this.notify.success(this.l('SuccessfullyDeleted'));
+                                });
+                        }
+                    }
+                );
+            });
+        }
+    }
+
+    exportToExcel(): void {
+        this._pmsgroupService.getPMSgroupsToExcel()
+            .subscribe(result => {
+                this._fileDownloadService.downloadTempFile(result);
+            });
+    }
+
+    createPMSgroup(): void {
+        this.editModal.show();
+    }
 }
